aa25a6901e6d923d94249cb7dae6ff0d3b397af2
hensell de la rosa <henseldelarosa@DESKTOP-DOB1QI3.localdomain>
2023-09-02 23:53:49 -0400

    problem 15 j

---
+++ JS/15-problem.js
@@ -0,0 +1,41 @@
+const threeSum = (nums) =>{
+  nums.sort((a,b) => a - b)
+  let result = []
+
+  for(let i = 0; i < nums.length; i++){
+      if (i > 0 && nums[i] === nums[i -1]) continue
+
+      const target = 0 - nums[i]
+
+      let left = i + 1
+
+      let right = nums.length -1
+
+      while(right >left){
+          const sum = nums[left] + nums[right]
+
+          if(sum > target){
+              right--
+          }
+          else if (sum < target){
+              left++
+          }
+          else{
+              result.push([nums[i], nums[left], nums[right]])
+              while(nums[left] === nums[left + 1] ) left++
+              while(nums[right] === nums[right - 1] ) right--
+              left ++
+              right --
+          }
+      }
+  }
+  return result
+}
+
+
+// example 1
+console.log(threeSum([-1,0,1,2,-1,-4])) //[[-1,-1,2],[-1,0,1]]
+// example 1
+console.log(threeSum([0,1,1])) //[]
+// example 1
+console.log(threeSum([0,0,0])) //[[0,0,0]]
