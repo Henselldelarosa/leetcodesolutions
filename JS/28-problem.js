7f62131c9f077e18b5eaf8d953b68adfa0783cd6
hensell de la rosa <henseldelarosa@DESKTOP-DOB1QI3.localdomain>
2023-09-10 14:19:59 -0400

    problem 28 js

---
+++ JS/28-problem.js
@@ -0,0 +1,9 @@
+const strStr = (haystack, needle) => {
+  return haystack.indexOf(needle)
+};
+
+
+//example1
+console.log(strStr('sadbutsad', 'sad')) //0
+//example2
+console.log(strStr('leetcode', 'leeto')) //-1
